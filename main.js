6327d46d0d8c7d43337bd9760667c879f93bd7cb
valentin <gritv85@gmail.com>
2020-01-09 19:45:56 +0100

    Doc events

--- main.js
+++ main.js
@@ -2,14 +2,19 @@ const { Client } = require("discord.js");
 const client = new Client({ disableEveryone: true });
 const { TOKEN, PREFIX } = require("./config");
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
 client.on("message", msg => {
   if (msg.author.bot) return;
   const args = msg.content.split(/ +/g);
   const cmd = args.shift().toLocaleLowerCase();
 });
 
+client.on("guildMemberAdd", member => { member.send("salut à toi !"); });
+
 client.login(TOKEN);
+
+client.on("ready", () => console.log(`Logged in as ${client.user.tag}!`));
+client.on("error", console.error);
+client.on("warn", console.warn);
+client.on("debug", console.log);
+
+
