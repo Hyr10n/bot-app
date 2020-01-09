d8dd978c9f53fa131f7c3eae144fe52773d42fd3
valentin <gritv85@gmail.com>
2020-01-10 00:36:48 +0100

    commande info

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-const { Client } = require("discord.js");
+const { Client, MessageEmbed } = require("discord.js");
 const client = new Client({ disableEveryone: true });
 const { TOKEN, PREFIX } = require("./config");
 
 client.on("message", msg => {
   if (msg.author.bot) return;
-  const args = msg.content.split(/ +/g);
+  if (msg.content.indexOf(PREFIX) !== 0) return;
+  const args = msg.content
+    .slice(PREFIX.length)
+    .trim()
+    .split(/ +/g);
   const cmd = args.shift().toLocaleLowerCase();
+  if (cmd === "info") {
+    const embed = new MessageEmbed()
+      .setDescription(msg.author.tag, msg.author.avatarURL())
+      .addField("Membre", msg.guild.memberCount, true)
+      .addField("Owner", msg.guild.owner.user.tag, true)
+      .setImage(msg.guild.iconURL())
+      .setTimestamp();
+    msg.channel.send(embed);
+  }
 });
 
-client.on("guildMemberAdd", member => { member.send("salut à toi !"); });
+client.on("guildMemberAdd", member => {
+  member.send("salut à toi !");
+});
 
 client.login(TOKEN);
 
@@ -16,5 +31,3 @@ client.on("ready", () => console.log(`Logged in as ${client.user.tag}!`));
 client.on("error", console.error);
 client.on("warn", console.warn);
 client.on("debug", console.log);
-
-
