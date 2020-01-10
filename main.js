c0bf11c9cac067a6ddd9ff51c820db5f0ccc811a
valentin <gritv85@gmail.com>
2020-01-10 11:46:58 +0100

    mongoDB

--- main.js
+++ main.js
@@ -4,13 +4,18 @@ const client = new Client({ disableEveryone: true });
 
 client.PREFIX = PREFIX;
 
+client.mongoose = require("./util/mongoose");
+
 client.commands = new Collection();
 client.commands.set("sinfo", require("./commands/sinfo.js"));
 
 client.on("ready", () => require("./events/ready.js")(client));
 client.on("message", msg => require("./events/message.js")(client, msg));
-client.on("guildMemberAdd", member => require("./events/guildMemberAdd.js")(client, member));
+client.on("guildMemberAdd", member =>
+  require("./events/guildMemberAdd.js")(client, member)
+);
 
+client.mongoose.init();
 client.login(TOKEN);
 client.on("error", console.error);
 client.on("warn", console.warn);
