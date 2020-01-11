47ce8af8b7b9e568d578a80d608f34e76e148f29
valentin <gritv85@gmail.com>
2020-01-11 14:34:10 +0100

    jsp

--- main.js
+++ main.js
@@ -3,16 +3,20 @@ const { TOKEN, PREFIX } = require("./config");
 const client = new Client({ disableEveryone: true });
 
 client.PREFIX = PREFIX;
-
+require("./util/function")(client);
 client.mongoose = require("./util/mongoose");
-
 client.commands = new Collection();
+
 client.commands.set("sinfo", require("./commands/sinfo.js"));
+client.commands.set("eval", require("./commands/eval.js"));
 
 client.on("ready", () => require("./events/ready.js")(client));
 client.on("message", msg => require("./events/message.js")(client, msg));
 client.on("guildMemberAdd", member =>
   require("./events/guildMemberAdd.js")(client, member)
+);
+client.on("guildCreate", guild =>
+  require("./events/guildCreate.js")(client, guild)
 );
 
 client.mongoose.init();
